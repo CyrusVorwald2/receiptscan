d92cb0bca4f7a1f1974da99985800d45b39e53de
Alec Beri <aberi@umd.edu>
2015-10-03 14:53:14 -0400

    Fixed examples

--- transform_example.py
+++ transform_example.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # USAGE
-# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]" --outputfile='example_01.warped.png"
-# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]" --outputfile='example_02.warped.png"
-# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]" --outputfile='example_03.warped.png" 
+# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]" --outputfile='example_01.warped.png'
+# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]" --outputfile='example_02.warped.png'
+# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]" --outputfile='example_03.warped.png'
 
 # import the necessary packages
 from pyimagesearch.transform import four_point_transform
