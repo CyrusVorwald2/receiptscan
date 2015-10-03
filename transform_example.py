de7ee40fa068254a0a2c55f86e037fd28bce7f7a
Alec Beri <aberi@umd.edu>
2015-10-03 14:47:57 -0400

    Updated usage examples

--- transform_example.py
+++ transform_example.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # USAGE
-# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]"
-# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]"
-# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]"
+# python transform_example.py --image images/example_01.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]" --outputfile='example_01.warped.png"
+# python transform_example.py --image images/example_02.png --coords "[(101, 185), (393, 151), (479, 323), (187, 441)]" --outputfile='example_02.warped.png"
+# python transform_example.py --image images/example_03.png --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]" --outputfile='example_03.warped.png" 
 
 # import the necessary packages
 from pyimagesearch.transform import four_point_transform
@@ -15,6 +15,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", help = "path to the image file")
 ap.add_argument("-c", "--coords",
 	help = "comma seperated list of source points")
+ap.add_argument("-o", "--outputfile")
 args = vars(ap.parse_args())
 
 # load the image and grab the source coordinates (i.e. the list of
@@ -33,5 +34,5 @@ warped = four_point_transform(image, pts)
 cv2.imshow("Original", image)
 cv2.imshow("Warped", warped)
 
-cv2.imwrite("warped_image.jpg", warped)
+cv2.imwrite(args["outputfile"], warped)
 cv2.waitKey(0)
